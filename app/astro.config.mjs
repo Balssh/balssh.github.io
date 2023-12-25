e9f382fe3c326310489d91a5b0d47f74f42dcf73
balssh <george.bals25@gmail.com>
2023-12-26 00:00:51 +0200

    finished the tutorial

--- app/astro.config.mjs
+++ app/astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from "astro/config";
 
+import solidJs from "@astrojs/solid-js";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://balssh.github.io",
-  base: "/balssh.github.io",
+  site: "https://balssh.github.io"
+  // base: "/balssh.github.io",
+  ,
+  integrations: [solidJs()]
 });
