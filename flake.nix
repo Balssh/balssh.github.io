ee1b2f7f322accac3bcb901867eb6def178956c1
Balssh <george.bals25@gmail.com>
2024-10-21 21:59:11 +0300

    testing things

--- flake.nix
+++ flake.nix
@@ -4,71 +4,77 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    ...
-  }: let
-    forAllSystems = nixpkgs.lib.genAttrs ["x86_64-linux"];
+  outputs =
+    {
+      self,
+      nixpkgs,
+      ...
+    }:
+    let
+      forAllSystems = nixpkgs.lib.genAttrs [ "x86_64-linux" ];
 
-    pkgsForSystem = system: (import nixpkgs {inherit system;});
-  in {
-    packages = forAllSystems (
-      system: let
-        inherit
-          (pkgsForSystem system)
-          buildEnv
-          cacert
-          dockerTools
-          zola
-          lib
-          ;
-        version = self.shortRev or (builtins.substring 0 7 self.dirtyRev);
-        rev = self.rev or self.dirtyRev;
-      in {
-        default = self.packages.${system}.balssh;
-
-        balssh-container = dockerTools.buildImage {
-          name = "balssh/jnsgr.uk";
-          tag = version;
-          created = "now";
-          copyToRoot = buildEnv {
-            name = "image-root";
-            paths = [
-              self.packages.${system}.balssh
-              cacert
-            ];
-            pathsToLink = [
-              "/bin"
-              "/etc/ssl/certs"
-            ];
-          };
-          config = {
-            Entrypoint = ["${lib.getExe self.packages.${system}.balssh}"];
-            Expose = [
-              8080
-              8801
-            ];
-            User = "10000:10000";
-          };
-        };
-      }
-    );
-
-    devShells = forAllSystems (
-      system: let
-        pkgs = pkgsForSystem system;
-      in {
-        default = pkgs.mkShell {
-          name = "balssh";
-          NIX_CONFIG = "experimental-features = nix-command flakes";
-          nativeBuildInputs = with pkgs; [
+      pkgsForSystem = system: (import nixpkgs { inherit system; });
+    in
+    {
+      packages = forAllSystems (
+        system:
+        let
+          inherit (pkgsForSystem system)
+            buildEnv
+            cacert
+            dockerTools
             zola
-            zsh
-          ];
-          shellHook = "exec zsh";
-        };
-      }
-    );
-  };
+            lib
+            ;
+          version = self.shortRev or (builtins.substring 0 7 self.dirtyRev);
+          rev = self.rev or self.dirtyRev;
+        in
+        {
+          default = self.packages.${system}.balssh;
+
+          balssh-container = dockerTools.buildImage {
+            name = "balssh/jnsgr.uk";
+            tag = version;
+            created = "now";
+            copyToRoot = buildEnv {
+              name = "image-root";
+              paths = [
+                self.packages.${system}.balssh
+                cacert
+              ];
+              pathsToLink = [
+                "/bin"
+                "/etc/ssl/certs"
+              ];
+            };
+            config = {
+              Entrypoint = [ "${lib.getExe self.packages.${system}.balssh}" ];
+              Expose = [
+                8080
+                8801
+              ];
+              User = "10000:10000";
+            };
+          };
+        }
+      );
+
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = pkgsForSystem system;
+        in
+        {
+          default = pkgs.mkShell {
+            name = "balssh";
+            NIX_CONFIG = "experimental-features = nix-command flakes";
+            nativeBuildInputs = with pkgs; [
+              zola
+              zsh
+            ];
+            shellHook = "exec zsh";
+          };
+        }
+      );
+    };
 }
